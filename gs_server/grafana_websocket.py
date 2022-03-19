be1ec2c4a378940d00b630723cac8adb53b7c8b5
Tom Danvers <tbodanvers1@sheffield.ac.uk>
2022-03-19 13:46:17 +0000

    add GrafanaLiveOutput and FakeSerialInput

---
+++ gs_server/grafana_websocket.py
@@ -0,0 +1,57 @@
+import asyncio
+
+import websockets
+from typing import List, Tuple
+from logging import info
+
+
+class GrafanaLiveOutput:
+    url: str
+    auth_token: str
+    headers: List[Tuple[str, str]]
+
+    ws: websockets.WebSocketClientProtocol
+
+    connected: bool = False
+
+    def __init__(self, url: str, auth_token: str):
+        self.url = url
+        self.headers = [
+            ("Authorization", f"Bearer {auth_token}")
+        ]
+
+    async def connect(self):
+        while not self.connected:
+            try:
+                info(f"Connecting to url {self.url}")
+                self.ws = await websockets.connect(self.url, extra_headers=self.headers)
+            except websockets.InvalidURI as e:
+                info(e)
+                break
+            except (websockets.InvalidHandshake, TimeoutError, ConnectionRefusedError) as e:
+                info(e)
+                info(f"Attempting to reconnect to url {self.url} in 5 seconds")
+                await asyncio.sleep(5)
+            else:
+                info(f"Connection successful")
+                self.connected = True
+
+        asyncio.create_task(self.listen())
+
+    async def listen(self):
+        while self.connected:
+            try:
+                await self.ws.recv()  # raises ConnectionClosed if the connection is lost
+            except websockets.ConnectionClosed:
+                info(f"Lost connection to url {self.url}")
+                self.connected = False
+                asyncio.create_task(self.connect())
+
+    async def output(self, output_string: str):
+        if self.connected:
+            try:
+                await self.ws.send(output_string)
+            except websockets.ConnectionClosed:
+                info(f"Lost connection to url {self.url}")
+                self.connected = False
+                asyncio.create_task(self.connect())
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from gs_server.serial import SerialLineProtocolInput
+import logging
+
+from gs_server.serial import SerialLineInput, FakeSerialLineInput
+from gs_server.grafana_websocket import GrafanaLiveOutput
 import asyncio
+
+logging.basicConfig(level=logging.INFO)
 
 
 async def my_output(output_str: str):
-    print(output_str)
+    # print(output_str)
+    pass
 
 
-serial_input = SerialLineProtocolInput("COM4", 9600)
+# serial_input = SerialLineInput("COM4", 9600)
+serial_input = FakeSerialLineInput()
 serial_input.add_output(my_output)
 
+grafana_output = GrafanaLiveOutput(
+    url="ws://127.0.0.1:3000/api/live/push/test_stream_id",
+    auth_token="eyJrIjoic2xET3E3aTRZOGE2RjNGdk54MUc5ZGU0TEE0SWpnZk4iLCJuIjoidGVzdCIsImlkIjoxfQ=="
+)
+serial_input.add_output(grafana_output.output)
+
 loop = asyncio.get_event_loop()
+loop.create_task(grafana_output.connect())
 loop.create_task(serial_input.connect())
 loop.run_forever()
